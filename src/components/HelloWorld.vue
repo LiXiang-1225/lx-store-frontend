b25b8c0ef46ce61a78bf6af05f565a87fe15c7d2
xiang.li5 <xiang.li5@thoughtworks.com>
2023-03-15 11:30:08 +0800

    使用组合式api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,29 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import {ref} from 'vue'
+const props = defineProps({
+  msg: String
+})
 
-defineProps<{ msg: string }>()
+const count = ref(0);
+const input = ref('')
 
-const count = ref(0)
+function plus() {
+  console.log(1)
+  count.value++
+}
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ props.msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <button type="button" @click=plus>add 1</button>
+    <p>{{ count }}</p>
+    <input v-model="input">
+    <div>{{ input }}</div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
 </style>
